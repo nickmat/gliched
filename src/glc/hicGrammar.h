642d522b5c6b3b966c439fada5568b891880bc2f
Nick Matthews <nick@kizzyco.com>
2023-04-18 16:58:54 +0100

    Add calculate field names and script to Grammar class.

--- src/glc/hicGrammar.h
+++ src/glc/hicGrammar.h
@@ -50,12 +50,18 @@ namespace glich {
 
         void set_base_fieldnames( StdStrVec fieldnames ) { m_base_fieldnames = fieldnames; }
         void set_opt_fieldnames( StdStrVec fieldnames ) { m_opt_fieldnames = fieldnames; }
+        void set_calc_fieldnames( StdStrVec fieldnames ) { m_calc_fieldnames = fieldnames; }
+        void set_rank_fieldnames( StdStrVec fieldnames ) { m_rank_fieldnames = fieldnames; }
         void set_preferred( const std::string& fcode );
         void add_lexicon( Lexicon* lex ) { m_lexicons.push_back( lex ); }
         FormatText* create_format_text( const std::string& code );
         FormatUnit* create_format_unit( const std::string& code );
         bool add_format( Format* fmt );
         void add_alias( const std::string& alias, const StdStrVec& pairs );
+        void set_calculate( const std::string& input, const std::string& output ) {
+            m_calculate_input = input;
+            m_calculate_output = output;
+        }
 
         std::string get_code() const { return m_code; }
         std::string resolve_field_alias( const std::string& alias );
@@ -72,6 +78,7 @@ namespace glich {
 
         StdStrVec get_base_fieldnames() const { return m_base_fieldnames; }
         StdStrVec get_opt_fieldnames() const { return m_opt_fieldnames; }
+        StdStrVec get_calc_fieldnames() const { return m_calc_fieldnames; }
         Glich& get_glich() const { return *m_glc; }
         void remove_format( const std::string& fcode ) { m_formats.erase( fcode ); }
 
@@ -91,10 +98,14 @@ namespace glich {
         std::string m_pref_output_fcode;
         StdStrVec   m_base_fieldnames;
         StdStrVec   m_opt_fieldnames;
+        StdStrVec   m_calc_fieldnames;
+        StdStrVec   m_rank_fieldnames;
         StdStrMap   m_field_alias;
         StdStrMap   m_num_pseudo_alias;
         StdStrMap   m_unit_alias;
         StrUnitMap  m_unit_type_alias;
+        std::string m_calculate_input;
+        std::string m_calculate_output;
     };
 
 }

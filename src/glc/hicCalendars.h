3d9fb7180e429a0e13625449514bb1ba8903ba82
Nick Matthews <nick@kizzyco.com>
2023-11-14 13:39:36 +0000

    Add convenience header to include all calendar classes.

---
+++ src/glc/hicCalendars.h
@@ -0,0 +1,43 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Name:        src/glc/hicCalendars.h
+ * Project:     Glich: Extendable Script Language.
+ * Purpose:     Include all calendar classes.
+ * Author:      Nick Matthews
+ * Website:     https://github.com/nickmat/glich
+ * Created:     14th November 2023
+ * Copyright:   Copyright (c) 2023, Nick Matthews.
+ * Licence:     GNU GPLv3
+ *
+ *  Glich is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Glich is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Glich.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+*/
+
+#ifndef GLC_HICCALENDAR_H_GUARD
+#define GLC_HICCALENDAR_H_GUARD
+
+#include "hicChinese.h"
+#include "hicFrench.h"
+#include "hicGregorian.h"
+#include "hicHebrew.h"
+#include "hicHybrid.h"
+#include "hicIslamic.h"
+#include "hicIsoOrdinal.h"
+#include "hicIsoWeek.h"
+#include "hicJdn.h"
+#include "hicJulian.h"
+#include "hicJwn.h"
+
+#endif // GLC_HICCALENDAR_H_GUARD

a0db7fb14690b6f91b30def8cca5b8133aa06e1c
Nick Matthews <nick@kizzyco.com>
2025-04-27 16:45:19 +0100

    Test glich all tests batch file corrected.
    Used the wrong directory for test script files.

--- test/runAllR.bat
+++ test/runAllR.bat
@@ -3,6 +3,6 @@ echo Glich Unit Tests Release
 ..\build\vc-working\Release\glcunit.exe
 echo.
 cd ..\3rdparty\glich\test\glcscripts\suite
-..\..\..\..\..\build\vc-working\Release\glctest.exe suite
+..\..\..\..\..\build\vc-working\Release\glctest.exe .\
 echo.
 pause
